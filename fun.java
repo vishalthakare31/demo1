39cf90f73cc07e2b1e1619ef0d0f4ae59d0fabd1
vishalthakare31 <101618202+vishalthakare31@users.noreply.github.com>
2022-03-15 12:54:29 +0530

    Add files via upload

---
+++ fun.java
@@ -0,0 +1,20 @@
+class Fun{
+
+    static void add(int a,int b)
+{
+
+        int result=a+b;
+
+        System.out.println("sum is "+result);
+
+     }
+
+
+
+    public static void main(String ar[]) {
+
+       add(6,7);          
+
+    }
+
+}
